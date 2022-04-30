10b91a48311489fa2fcb9f0790f5848a7f433f19
Jake Baines <jake_baines@rapid7.com>
2022-04-30 06:05:52 -0700

    Gross changes but repacking a package yields the exact original

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <unistd.h>
 #include <map>
+#include <set>
 #include <filesystem>
 
 /*
@@ -108,6 +109,7 @@ namespace
         asdm.erase(0, 4);
 
         std::map<std::string, std::pair<std::uint32_t, std::uint32_t> > entries;
+        std::set<std::uint32_t> offsets;
 
         for (std::uint32_t i = 0; i < manifest_count; i++)
         {
@@ -119,11 +121,23 @@ namespace
             std::uint32_t data_offset = *reinterpret_cast<std::uint32_t*>(entry_header.data());
             std::uint32_t data_size = *reinterpret_cast<std::uint32_t*>(entry_header.data() + 4);
             std::uint32_t entry_length = *reinterpret_cast<std::uint32_t*>(entry_header.data() + 8);
+
+            bool duplicate = false;
+            if (offsets.find(data_offset) == offsets.end())
+            {
+                offsets.insert(data_offset);
+            }
+            else
+            {
+                duplicate = true;
+            }
+
             std::cout << "\t-> Entry length: " << entry_length << std::endl;
             std::string filename(asdm.data(), asdm.data() + entry_length);
             std::cout << "\t-> Entry file: " << filename << std::endl;
             std::cout << "\t-> Data offset: " << data_offset << std::endl;
             std::cout << "\t-> Data size: " << data_size << std::endl;
+            std::cout << "\t-> Duplicate: " << duplicate << std::endl;
             asdm.erase(0, entry_length);
 
             entries[filename] = std::make_pair(data_offset, data_size);
@@ -159,7 +173,6 @@ namespace
         std::uint32_t m_name_length;
     };
     #pragma pack(pop)
-    
 
     bool do_repackage(const std::string& p_input_file)
     {
@@ -170,17 +183,25 @@ namespace
         }
 
         std::string temp_entries;
+
+        // this is a dump datastructure to use. It assume the files are read in alphabetical order
         std::map<std::string, manifest_entry> entries;
 
         std::uint32_t entry_count = 0;
         std::cout << "-> Sizeof manifest entry " << std::hex << (int)sizeof(manifest_entry) << std::endl;
         std::cout << "-> Files to package " << std::endl;
-        for(auto it = std::filesystem::directory_iterator(p_input_file); it != std::filesystem::directory_iterator(); ++it)
+
+        // the files are written in, mostly, alphabetical order
+        std::vector<std::filesystem::path> files_in_directory;
+        std::copy(std::filesystem::directory_iterator(p_input_file), std::filesystem::directory_iterator(), std::back_inserter(files_in_directory));
+        std::sort(files_in_directory.begin(), files_in_directory.end());
+
+        for (const std::filesystem::path& filename : files_in_directory)
         {
             entry_count++;
 
             // 1 byte 0, 4 bytes offset, 4 bytes length, 4 bytes name length
-            std::string name((*it).path().string());
+            std::string name(filename.string());
             name.erase(0, p_input_file.size());
 
             // replace safe character
@@ -191,6 +212,17 @@ namespace
             }
 
             std::cout << "\t-> " << name << std::endl;
+
+            if ((name.size() % 0x10) == 0|| name.size() > 0x10)
+            {
+                // wat
+                name.push_back('\x00');
+            }
+
+            if (name == "version.prop")
+            {
+                name.push_back('\x00');
+            }
 
             // pad on 4 byte boundary
             while ((name.size() % 4) != 0)
@@ -205,6 +237,8 @@ namespace
             }
 
             memset(&entries[name], 0, sizeof(manifest_entry));
+            std::cout << name.size() << std::endl;
+            std::cout << name << std::endl;
             entries[name].m_name_length = name.size();
             temp_entries.append(reinterpret_cast<char*>(&entries[name]), sizeof(manifest_entry));
             temp_entries.append(name);
@@ -228,43 +262,72 @@ namespace
         std::uint32_t data_offset = sizeof(header) + sizeof(hash) + compilation_date.size() + sizeof(entry_count) + temp_entries.size();
 
         std::string data;
-        for(auto it = std::filesystem::directory_iterator(p_input_file); it != std::filesystem::directory_iterator(); ++it)
+        std::string asdm_entry;
+        for (const std::filesystem::path& filename : files_in_directory)
         {
-              std::string name((*it).path().string());
-              std::string file_data(load_file(name));
-              if (file_data.empty())
-              {
-                  std::cerr << "Failed to load a  file for packing" << std::endl;
-                  return false;
-              }
+            std::string name(filename.string());
+            std::string file_data(load_file(name));
+            if (file_data.empty())
+            {
+                std::cerr << "Failed to load a  file for packing" << std::endl;
+                return false;
+            }
 
-              name.erase(0, p_input_file.size());
+            name.erase(0, p_input_file.size());
 
-              // replace safe character
-              while (name.find("%") != std::string::npos)
-              {
-                  std::size_t index = name.find("%");
-                  name.replace(index, 1, "/");
-              }
-              while (name.find("@") != std::string::npos)
-              {
-                  std::size_t index = name.find("@");
-                  name.replace(index, 1, ".");
-              }
+            // replace safe character
+            while (name.find("%") != std::string::npos)
+            {
+                std::size_t index = name.find("%");
+                name.replace(index, 1, "/");
+            }
+            while (name.find("@") != std::string::npos)
+            {
+                std::size_t index = name.find("@");
+                name.replace(index, 1, ".");
+            }
 
-              // pad on 4 byte boundary
-              while ((name.size() % 4) != 0)
-              {
-                  // the +1 logic is fucked. likely i fucked up the struct
-                  name.push_back('\x00');
-              }
+            if ((name.size() % 0x10) == 0 || name.size() > 0x10)
+            {
+                // wat
+                name.push_back('\x00');
+            }
 
-              std::uint32_t length = file_data.size();
-              entries[name].m_data_length = length;
-              entries[name].m_data_offset = data_offset;
-              std::cout << "\t->Offset (" << name << ")" << data_offset << std::endl;
-              data_offset += file_data.size();
-              data.append(file_data.data(), file_data.size());
+            if (name == "version.prop")
+            {
+                name.push_back('\x00');
+            }
+
+            // pad on 4 byte boundary
+            while ((name.size() % 4) != 0)
+            {
+                // the +1 logic is fucked. likely i fucked up the struct
+                name.push_back('\x00');
+            }
+
+            std::uint32_t length = file_data.size();
+            entries[name].m_data_length = length;
+            entries[name].m_data_offset = data_offset;
+            std::cout << "\t->Offset (" << name << ") (" << data_offset << " -> " << (data_offset + file_data.size()) << ")" << std::endl;
+            
+            if (name.starts_with("dm-launcher.msi"))
+            {
+                entries[asdm_entry].m_data_offset = data_offset;
+            }
+            if (!name.starts_with("asdm50-install.msi"))
+            {
+                data_offset += file_data.size();
+                data.append(file_data.data(), file_data.size());
+                while ((data.size() % 4) != 0)
+                {
+                    data.push_back('\x00');
+                    ++data_offset;
+                }
+            }
+            else
+            {
+                asdm_entry.assign(name);
+            }
         }
 
         // compute total length
